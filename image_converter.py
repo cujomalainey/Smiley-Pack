f1e401cb74ba25ef51633fd38ab935c9775d9b8c
Curtis Malainey <curtis@malainey.com>
2015-09-25 11:48:50 -0600

    added image loading and sample image

--- image_converter.py
+++ image_converter.py
@@ -1,1 +1,16 @@
 import sys
+from PIL import Image
+
+if len(sys.argv) != 2:
+	exit("No file specified")
+
+filename = sys.argv[1]
+
+im = Image.open(filename) #Can be many different formats.
+pix = im.load()
+
+if im.size != (8, 8):
+	exit("Image with incorrect Dimensions")
+
+print(pix[0,0]) #Get the RGBA Value of the a pixel of an image
+# pix[x,y] = value # Set the RGBA Value of the image (tuple)
