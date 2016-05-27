e2d6e3a353ab685f521a7d292e24a3d62bdf1873
Curtis Malainey <curtis@malainey.com>
2016-05-26 19:44:44 -0600

    New Loading Procedure, cuts back RAM big time

--- src/Smiley-Pack.cpp
+++ src/Smiley-Pack.cpp
@@ -6,14 +6,11 @@
 #define MATRIX_HEIGHT   8
 #define MATRIX_WIDTH    8
 #define NUMPIXELS       MATRIX_WIDTH * MATRIX_HEIGHT
+#define FRAME_SIZE      NUMPIXELS * 3
 #define PIN             9
 #define FRAMES          10
 #define MAX_BRIGHTNESS  255.0
 #define CAP_BRIGHTNESS  50
-
-enum color { RED, GREEN, BLUE, RGB_COUNT };
-
-uint8_t array[FRAMES][MATRIX_HEIGHT][MATRIX_WIDTH][3];
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -21,11 +18,13 @@ const uint16_t levels[] = {20, 50, 75, 100, 150, 200, 250, 300, 350, 400, 425};
 
 uint8_t frame_index = FRAMES - 1;
 
-void update_leds(uint8_t frame[MATRIX_HEIGHT][MATRIX_WIDTH][3]);
+File file;
 
 void clear_leds();
 
 void flame();
+
+void update_leds(uint8_t frame);
 
 void setup() {
 	pixels.begin();
@@ -33,28 +32,17 @@ void setup() {
 	while (!Serial) {;}
 	SD.begin();
 	Serial.println("Loading File");
-	File file = SD.open("img.dat");
+	file = SD.open("img.dat");
 	if (!file) {
 		Serial.println("File failed to load");
 		return;
-	}
-	for (uint8_t f = 0; f < FRAMES; f++) {
-		Serial.print("Loading Frame: ");
-		Serial.println(f);
-		for (uint8_t x = 0; x < MATRIX_WIDTH; x++) {
-			for (uint8_t y = 0; y < MATRIX_HEIGHT; y++) {
-				for (uint8_t c = 0; c < RGB_COUNT; c++) {
-					array[f][x][y][c] = file.read();
-				}
-			}
-		}
 	}
 	randomSeed(analogRead(0));
 }
 
 void loop() {
 	delay(500);
-	int val = analogRead(0);
+	uint16_t val = analogRead(0);
   	Serial.print("analog 0 is: ");
   	Serial.println(val);
   	if (frame_index == 0 && val < levels[frame_index]) {
@@ -72,26 +60,35 @@ void loop() {
   	else if (val < levels[frame_index - 1]) {
   		frame_index--;
   	}
-	Serial.print("Displaying Frame: ");
-	Serial.println(frame_index);
 	if (frame_index < 0) {
 		clear_leds();
 	}
 	else {
-		update_leds(array[frame_index]);
+		update_leds(frame_index);
 	}
 }
 
-void update_leds(uint8_t frame[MATRIX_HEIGHT][MATRIX_WIDTH][3]) {
+void update_leds(uint8_t frame) {
+	Serial.print("Loading Frame: ");
+	Serial.println(frame);
+	file.seek(frame*FRAME_SIZE);
+	uint16_t t = millis();
 	for (int y = 0; y < MATRIX_HEIGHT; y++)
 	{
 		for (int x = 0; x < MATRIX_WIDTH; x++)
 		{
+			uint8_t red   = file.read();
+			uint8_t green = file.read();
+			uint8_t blue  = file.read();
 			pixels.setPixelColor(y*MATRIX_WIDTH + x,
-      			pixels.Color((frame[y][x][RED]/MAX_BRIGHTNESS) * CAP_BRIGHTNESS, (frame[y][x][GREEN]/MAX_BRIGHTNESS) * CAP_BRIGHTNESS, (frame[y][x][BLUE]/MAX_BRIGHTNESS) * CAP_BRIGHTNESS)
+      			pixels.Color((red/MAX_BRIGHTNESS) * CAP_BRIGHTNESS, (green/MAX_BRIGHTNESS) * CAP_BRIGHTNESS, (blue/MAX_BRIGHTNESS) * CAP_BRIGHTNESS)
       			);
 		}
 	}
+	t = millis() - t;
+	Serial.print("Took: ");
+	Serial.println(t);
+	Serial.println("Displaying.");
 	pixels.show();
 }
 
